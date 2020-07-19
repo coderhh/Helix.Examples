bfbf36a958b051b8cad86f7ed604e4d3de88b24f
coderhh <yehanghan@gmail.com>
2020-07-19 19:56:52 +0800

    update clean data script to keep _DEV

--- examples/helix-basic-tds-with-docker-vs2019-project-type/Docker/Clean-Data.ps1
+++ examples/helix-basic-tds-with-docker-vs2019-project-type/Docker/Clean-Data.ps1
@@ -1,5 +1,17 @@
 Get-ChildItem -Path (Join-Path $PSScriptRoot "\data") -Directory | ForEach-Object {
     $dataPath = $_.FullName
-
+    
+    if($dataPath -like '*cm*'){
+        Get-ChildItem -Path $dataPath -Exclude ".gitkeep" | ForEach-Object {
+            if($_.FullName.Contains('wwwroot')){
+                Get-ChildItem -Path $_.FullName  -Recurse | ForEach-Object {
+                    if(!$_.FullName.Contains('_DEV')){
+                        Remove-Item  -Path $_.FullName -Force -Recurse -Verbose
+                    }
+                }
+            }
+        }
+    }else{
     Get-ChildItem -Path $dataPath -Exclude ".gitkeep" -Recurse | Remove-Item -Force -Recurse -Verbose
+    }
 }
